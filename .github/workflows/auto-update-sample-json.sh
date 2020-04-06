e650a346fbf17bc540e5cb7f67e6f2e0c430a230
Connor Clark <cjamcl@google.com>
2020-04-06 10:54:44 -0700

    test

--- .github/workflows/auto-update-sample-json.sh
+++ .github/workflows/auto-update-sample-json.sh
@@ -1,8 +1,11 @@
-set +xe
+set +ex
 
 echo hello there
 
 date > blah.txt
+
+git config --global user.email "nobody@example.com"
+git config --global user.name "GitHub Action"
 
 git remote add github "https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git"
 git pull github ${GITHUB_REF} --ff-only
